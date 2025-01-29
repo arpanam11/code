9076e854ac894acf6312a91af27046c1cd233788
arpanam11 <arpanameshram11@gmail.com>
2025-01-29 14:55:35 +0530

    add pacakge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  const fetchData = async () => {
+    const response = await axios.get('https://fakestoreapi.com/products');
+    setProducts(response.data);
+    console.log(response.data)
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {products.map((product, index) => (
+          <div key={index}>
+            <h5>{product.title}</h5> 
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
